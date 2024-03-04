32d588b8de4a1d9f2dc339e13c8c5098f65c178b
Ali Reid <ali.reid@live.co.uk>
2024-03-04 14:18:43 +0000

    Initial commit

---
+++ src/SFA.DAS.ApprenticeApp.Pwa/Configuration/ApplicationConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using SFA.DAS.Http.Configuration;
+
+namespace SFA.DAS.ApprenticeApp.Pwa.Configuration;
+
+[ExcludeFromCodeCoverage]
+public class ApplicationConfiguration
+{
+    public ConnectionStringsConfiguration ConnectionStrings { get; set; } = new();
+
+    public AuthenticationConfiguration Authentication { get; set; } = new();
+
+    public string? ClientId { get; set; }
+
+    public string? MetadataAddress { get; set; }
+
+    public string? KeyDir { get; set; }
+
+    public string? TokenUri { get; set; }
+
+    public string? UserInfoUri { get; set; }
+
+    public string? ProductInfoHeaderValue { get; set; }
+
+    public string? CookieName { get; set; }
+}
+
+public class ConnectionStringsConfiguration
+{
+    public string RedisConnectionString { get; set; } = null!;
+    public string DataProtectionKeysDatabase { get; set; } = null!;
+}
+
+public class AuthenticationConfiguration
+{
+    public string MetadataAddress { get; set; } = null!;
+}
+
+public class OuterApiConfiguration : IApimClientConfiguration
+{
+    public string ApiBaseUrl { get; set; } = null!;
+
+    public string SubscriptionKey { get; set; } = null!;
+
+    public string ApiVersion { get; set; } = null!;
+}
