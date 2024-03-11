06381cd626c3438bbed5516e1fb168bfb80303b8
Ali Reid <ali.reid@live.co.uk>
2024-03-11 13:16:42 +0000

    Profile info and surface email address

--- src/SFA.DAS.ApprenticeApp.Domain/Models/Apprentice.cs
+++ src/SFA.DAS.ApprenticeApp.Domain/Models/Apprentice.cs
@@ -9,6 +9,7 @@ namespace SFA.DAS.ApprenticeApp.Domain.Models
         public string FirstName { get; set; } = null!;
         public DateTime? DateOfBirth { get; set; }
         public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
         public bool TermsOfUseAccepted { get; set; }
     }
 }
