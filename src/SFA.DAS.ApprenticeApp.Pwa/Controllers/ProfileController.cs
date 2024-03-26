d04edf0137ccc61c551610a9365abbf02230f796
Ali Reid <ali.reid@live.co.uk>
2024-03-26 09:01:39 +0000

    Profile work

---
+++ src/SFA.DAS.ApprenticeApp.Pwa/Controllers/ProfileController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeApp.Application;
+using SFA.DAS.ApprenticeApp.Domain.Interfaces;
+using SFA.DAS.ApprenticeApp.Pwa.ViewModels;
+
+namespace SFA.DAS.ApprenticeApp.Pwa.Controllers;
+
+public class ProfileController : Controller
+{
+    private readonly ILogger<ProfileController> _logger;
+    private readonly IOuterApiClient _client;
+
+    public ProfileController
+        (
+        ILogger<ProfileController> logger,
+        IOuterApiClient client
+        )
+    {
+        _logger = logger;
+        _client = client;
+    }
+
+    [Authorize]
+    public async Task<IActionResult> Index()
+    {
+        var apprenticeId = HttpContext.User?.Claims?.First(c => c.Type == Constants.ApprenticeIdClaimKey)?.Value;
+
+        if (!string.IsNullOrEmpty(apprenticeId))
+        {
+            var apprenticeDetails = await _client.GetApprenticeDetails(new Guid(apprenticeId));
+
+            if (apprenticeDetails != null)
+            {
+                return View(new ProfileViewModel() { Apprentice = apprenticeDetails.Apprentice, MyApprenticeship = apprenticeDetails.MyApprenticeship });
+            }
+
+            _logger.LogInformation($"Profile Index 'apprenticeDetails' is null for {apprenticeId}");
+        }
+
+        return RedirectToAction("Index", "Home");
+    }
+}
