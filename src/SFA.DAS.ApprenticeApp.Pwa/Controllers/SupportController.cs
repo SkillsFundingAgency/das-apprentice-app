e42b9cbe2945d995143b99cbb3380a39ccda2895
tdavande <tdavande@outlook.com>
2024-05-14 15:03:22 +0100

    Scaffolding of Support & Guidance pages

---
+++ src/SFA.DAS.ApprenticeApp.Pwa/Controllers/SupportController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.ApprenticeApp.Pwa.Controllers
+{
+    public class SupportController : Controller
+    {
+        public IActionResult LandingPage()
+        {
+            return View();
+        }
+        public IActionResult SecondLevelPage()
+        {
+            return View();
+        }
+        public IActionResult SavedArticlesPage()
+        {
+            return View();
+        }
+    }
+
+}
