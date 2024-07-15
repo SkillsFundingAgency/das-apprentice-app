a3386a41327766da7cd640f213032a79bd01fedc
Vini <vinibance@gmail.com>
2024-07-15 10:42:18 +0100

    added controller tests

---
+++ src/SFA.DAS.ApprenticeApp.Pwa.UnitTests/Controllers/Welcome/WelcomeViewData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace SFA.DAS.ApprenticeApp.Pwa.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Welcome";
+            return View();
+        }
+    }
+}
+
+
