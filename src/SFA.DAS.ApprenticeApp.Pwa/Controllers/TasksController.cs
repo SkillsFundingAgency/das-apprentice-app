0fdc05a3b79dafc9ac073d4958f2eb929c373591
tdavande <tdavande@outlook.com>
2024-07-22 16:32:17 +0100

    intial commit of branch - new controller and folder structure with view added

---
+++ src/SFA.DAS.ApprenticeApp.Pwa/Controllers/TasksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.ApprenticeApp.Pwa.Controllers
+{
+    public class TasksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
