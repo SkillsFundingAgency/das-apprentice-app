14a4e2089a7a78d1ed6ef28e8e9b700b7973ac68
Ali Reid <ali.reid@live.co.uk>
2024-07-23 19:05:55 +0100

    Test fix

--- src/SFA.DAS.ApprenticeApp.Pwa.UnitTests/Controllers/Home/WhenLoadingHomePage.cs
+++ src/SFA.DAS.ApprenticeApp.Pwa.UnitTests/Controllers/Home/WhenLoadingHomePage.cs
@@ -1,14 +1,13 @@
-﻿using AutoFixture.NUnit3;
+﻿using System;
+using System.Security.Claims;
+using AutoFixture.NUnit3;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
+using SFA.DAS.ApprenticeApp.Application;
 using SFA.DAS.ApprenticeApp.Pwa.Controllers;
 using SFA.DAS.Testing.AutoFixture;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.ApprenticeApp.Pwa.UnitTests.Controllers.Home
 {
@@ -17,6 +16,19 @@ namespace SFA.DAS.ApprenticeApp.Pwa.UnitTests.Controllers.Home
         [Test, MoqAutoData]
         public void Then_The_Homepage_Is_Loaded([Greedy] HomeController controller)
         {
+            var httpContext = new DefaultHttpContext();
+            var apprenticeId = Guid.NewGuid();
+            var apprenticeIdClaim = new Claim(Constants.ApprenticeIdClaimKey, apprenticeId.ToString());
+            var claimsPrincipal = new ClaimsPrincipal(new[] {new ClaimsIdentity(new[]
+        {
+            apprenticeIdClaim
+        })});
+            httpContext.User = claimsPrincipal;
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+
             var result = controller.Index() as ActionResult;
             result.Should().NotBeNull();
         }
