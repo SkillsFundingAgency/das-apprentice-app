135897bc5b3a0e5195bb62e9493d8d0f6cd93966
James King <hello@jamesmichaelking.com>
2024-08-09 15:34:32 +0100

    Frontend refactor

---
+++ src/SFA.DAS.ApprenticeApp.Pwa/Controllers/FrontendController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.ApprenticeApp.Pwa.Controllers;
+
+public class FrontendController : Controller
+{
+
+    public IActionResult Home()
+    {
+        return View();
+    }
+    public IActionResult Terms()
+    {
+        return View();
+    }
+
+    public IActionResult Welcome()
+    {
+        return View();
+    }
+
+    public IActionResult TasksNotStarted()
+    {
+        return View();
+    }
+
+    public IActionResult Tasks()
+    {
+        return View();
+    }
+
+    public IActionResult KSB()
+    {
+        return View();
+    }
+    public IActionResult Support()
+    {
+        return View();
+    }
+    public IActionResult Notifications()
+    {
+        return View();
+    }
+    public IActionResult Account()
+    {
+        return View();
+    }
+}
+
+
+
