b9b9e395dffb84fed8cec1beb08869c9ff3611b3
James King <hello@jamesmichaelking.com>
2024-08-12 22:48:57 +0100

    WIP

--- src/SFA.DAS.ApprenticeApp.Pwa/wwwroot/js/app.js
+++ src/SFA.DAS.ApprenticeApp.Pwa/wwwroot/js/app.js
@@ -42,11 +42,40 @@ if (appTabs) {
   });
 }
 
-function Overlay(container) {
-  this.container = container;
+function Overlay(link) {
+  this.link = link;
+  this.bodyClassName = "app-template__body--overlay-open";
+  this.overlay;
 }
 
-Overlay.prototype.init = function () {};
+Overlay.prototype.init = function () {
+  const target = this.link.hash;
+  this.overlay = document.getElementById(target.substring(1));
+  if (!this.overlay) {
+    return;
+  }
+  this.setupEvents();
+};
+
+Overlay.prototype.setupEvents = function () {
+  this.link.addEventListener("click", this.showOverlay.bind(this));
+  const close = this.overlay.querySelector(".app-overlay-header__link");
+  close.addEventListener("click", this.hideOverlay.bind(this));
+};
+
+Overlay.prototype.showOverlay = function (event) {
+  event.preventDefault();
+  document.body.classList.add(this.bodyClassName);
+  const firstInput = this.overlay.querySelector("input");
+  console.log(firstInput);
+  if (firstInput) {
+    firstInput.focus();
+  }
+};
+
+Overlay.prototype.hideOverlay = function () {
+  document.body.classList.remove(this.bodyClassName);
+};
 
 const appOverlays = document.querySelectorAll(`[data-module="app-overlay"]`);
 
