a64b88e63b2df15e213d2bcc44f3a9675b7f62c7
Ali Reid <ali.reid@live.co.uk>
2024-08-15 09:48:10 +0100

    Add in KSB controller

---
+++ src/SFA.DAS.ApprenticeApp.Pwa/Controllers/KsbController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.ApprenticeApp.Pwa.Controllers
+{
+    public class KsbController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
