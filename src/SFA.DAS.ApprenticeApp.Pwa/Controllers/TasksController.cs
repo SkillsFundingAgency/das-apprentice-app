b7905a07cb33e856f2b60ca7d8b20483e54e198d
CathyGroom <147840579+CathyGroom@users.noreply.github.com>
2024-08-19 16:21:26 +0100

    update delete task

--- src/SFA.DAS.ApprenticeApp.Pwa/Controllers/TasksController.cs
+++ src/SFA.DAS.ApprenticeApp.Pwa/Controllers/TasksController.cs
@@ -13,7 +13,6 @@ namespace SFA.DAS.ApprenticeApp.Pwa.Controllers
     {
         private readonly ILogger<TasksController> _logger;
         private readonly IOuterApiClient _client;
-        public const long ApprenticeshipId = 123;
 
         public TasksController
             (
@@ -35,8 +34,8 @@ namespace SFA.DAS.ApprenticeApp.Pwa.Controllers
             {
                 var apprenticeDetails = await _client.GetApprenticeDetails(new Guid(apprenticeId));
 
-                var taskTodoResult = await _client.GetApprenticeTasks(apprenticeDetails.MyApprenticeship.ApprenticeshipId, 0, new DateTime(DateTime.Now.Year, 1, 1), new DateTime(DateTime.Now.Year, 12, 31));
-                var taskDoneResult = await _client.GetApprenticeTasks(apprenticeDetails.MyApprenticeship.ApprenticeshipId, 1, new DateTime(DateTime.Now.Year, 1, 1), new DateTime(DateTime.Now.Year, 12, 31));
+                var taskTodoResult = await _client.GetApprenticeTasks(apprenticeDetails.MyApprenticeship.ApprenticeshipId, 0, new DateTime(DateTime.Now.Year, 1, 1), new DateTime(DateTime.Now.Year, 12, 12));
+                var taskDoneResult = await _client.GetApprenticeTasks(apprenticeDetails.MyApprenticeship.ApprenticeshipId, 1, new DateTime(DateTime.Now.Year, 1, 1), new DateTime(DateTime.Now.Year, 12, 12));
 
                 if (taskTodoResult == null || taskTodoResult.Tasks.Count == 0)
                 {
@@ -109,13 +108,14 @@ namespace SFA.DAS.ApprenticeApp.Pwa.Controllers
         {
             var apprenticeId = HttpContext.User?.Claims?.First(c => c.Type == Constants.ApprenticeIdClaimKey)?.Value;
 
-            if (!string.IsNullOrEmpty(apprenticeId))
-            {
-                if (task.TaskId == 0)
+                if (!string.IsNullOrEmpty(apprenticeId))
+                {
+                    var apprenticeDetails = await _client.GetApprenticeDetails(new Guid(apprenticeId));
+                    if (task.TaskId == 0)
                 {
                     string preMessage = $"Adding new task for apprentice with id {apprenticeId}";
                     _logger.LogInformation(preMessage);
-                    await _client.AddApprenticeTask(ApprenticeshipId, task);
+                    await _client.AddApprenticeTask(apprenticeDetails.MyApprenticeship.ApprenticeshipId, task);
                     string postMessage = $"Task added successfully for apprentice with id {apprenticeId}";
                     _logger.LogInformation(postMessage);
                     return Ok();
@@ -123,7 +123,7 @@ namespace SFA.DAS.ApprenticeApp.Pwa.Controllers
                 else {                     
                     string preMessage = $"Updating task with id {task.TaskId} for apprentice with id {apprenticeId}";
                     _logger.LogInformation(preMessage);
-                    await _client.UpdateApprenticeTask(ApprenticeshipId, task.TaskId, task);
+                    await _client.UpdateApprenticeTask(apprenticeDetails.MyApprenticeship.ApprenticeshipId, task.TaskId, task);
                     string postMessage = $"Task updated successfully with id {task.TaskId} for apprentice with id {apprenticeId}";
                     _logger.LogInformation(postMessage);
                     return Ok();
@@ -136,20 +136,27 @@ namespace SFA.DAS.ApprenticeApp.Pwa.Controllers
         [HttpDelete]
         public async Task<IActionResult> DeleteApprenticeTask(int taskId)
         {
-            if(taskId == 0)
+            if (taskId == 0)
             {
                 _logger.LogWarning("Task Id cannot be null or zero. Cannot delete task.");
                 return RedirectToAction("Index", "Tasks");
             }
 
-            string preMessage = $"Deleting task with id {taskId}";
-            _logger.LogInformation(preMessage);
+            var apprenticeId = HttpContext.User?.Claims?.First(c => c.Type == Constants.ApprenticeIdClaimKey)?.Value;
 
-            await _client.DeleteApprenticeTask(ApprenticeshipId, taskId);
-            string postMessage = $"Deleting task with id {taskId}";
-            _logger.LogInformation(postMessage);
+            if (!string.IsNullOrEmpty(apprenticeId))
+            {
+                var apprenticeDetails = await _client.GetApprenticeDetails(new Guid(apprenticeId));
+                string preMessage = $"Deleting task with id {taskId}";
+                _logger.LogInformation(preMessage);
 
-            return RedirectToAction("Index");
+                await _client.DeleteApprenticeTask(apprenticeDetails.MyApprenticeship.ApprenticeshipId, taskId);
+                string postMessage = $"Deleting task with id {taskId}";
+                _logger.LogInformation(postMessage);
+
+                return RedirectToAction("Index");
+            }
+            return Unauthorized();
         }
     }
 }
