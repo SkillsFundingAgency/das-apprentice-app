54840b5a3538802937a1eeb6d8a6eed113f1fae0
Ali Reid <ali.reid@live.co.uk>
2024-08-22 10:01:10 +0100

    Add and Edit page integration

--- src/SFA.DAS.ApprenticeApp.Pwa/ViewModels/AddTaskPageModel.cs
+++ src/SFA.DAS.ApprenticeApp.Pwa/ViewModels/AddTaskPageModel.cs
@@ -5,5 +5,6 @@ namespace SFA.DAS.ApprenticeApp.Pwa.ViewModels
     public class AddTaskPageModel
     {
         public ApprenticeTask? Task { get; set; }
+        public List<ApprenticeshipCategory>? Categories { get; set; }
     }
 }
