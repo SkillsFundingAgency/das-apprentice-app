6439c6d8fc43bf69b0521e5bdac79368448ad462
CathyGroom <147840579+CathyGroom@users.noreply.github.com>
2024-10-08 06:09:00 +0100

    move domainextensions to new class

--- src/SFA.DAS.ApprenticeApp.Pwa/AppStart/AuthenticationStartup.cs
+++ src/SFA.DAS.ApprenticeApp.Pwa/AppStart/AuthenticationStartup.cs
@@ -44,24 +44,5 @@ namespace SFA.DAS.ApprenticeApp.Pwa.AppStart
 
             services.AddHttpContextAccessor();
         }
-
-        public static class DomainExtensions
-        {
-            public static string GetDomain(string environment)
-            {
-                string text = environment.ToLower();
-                if ((text != "local"))
-                {
-                    if (text == "prd")
-                    {
-                        return "my.apprenticeships.education.gov.uk";
-                    }
-
-                    return environment.ToLower() + "-apprentice-app.apprenticeships.education.gov.uk";
-                }
-
-                return "";
-            }
-        }
     }
 }
---
+++ src/SFA.DAS.ApprenticeApp.Pwa/Authentication/DomainExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace SFA.DAS.ApprenticeApp.Pwa.Authentication
+{
+    public static class DomainExtensions
+    {
+        public static string GetDomain(string environment)
+        {
+            string text = environment.ToLower();
+            if ((text != "local"))
+            {
+                if (text == "prd")
+                {
+                    return "my.apprenticeships.education.gov.uk";
+                }
+
+                return environment.ToLower() + "-apprentice-app.apprenticeships.education.gov.uk";
+            }
+
+            return "";
+        }
+    }
+}
