db0f947ea01dbe7a558fdea4327e12b841d04fa4
tdavande <113100339+tdavande@users.noreply.github.com>
2024-10-29 10:01:02 +0000

    Notifications screen work

--- src/SFA.DAS.ApprenticeApp.Pwa/Controllers/NotificationsController.cs
+++ src/SFA.DAS.ApprenticeApp.Pwa/Controllers/NotificationsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeApp.Pwa.ViewModels;
 
 namespace SFA.DAS.ApprenticeApp.Pwa.Controllers
 {
@@ -6,7 +7,47 @@ namespace SFA.DAS.ApprenticeApp.Pwa.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var notifications = new List<NotificationViewModel>
+    {
+        new NotificationViewModel
+        {
+            Title = "What is OTJ training?",
+            Description = "Find out why off-the-job training is key to your apprenticeship.",
+            TimeAgo = "New - 4 hours ago",
+            IsNew = true,
+            LinkUrl = "#"
+        },
+        new NotificationViewModel
+        {
+            Title = "Your end-point assessment",
+            Description = "Learn about the EPA you’ll take at the end of the apprenticeship.",
+            TimeAgo = "4 hours ago",
+            IsNew = false,
+            LinkUrl = "#"
+        },
+        new NotificationViewModel
+        {
+            Title = "Submit assessment for first module",
+            Description = "Due 1 February, 2:30pm\nReminder set: 5 minutes before",
+            TimeAgo = "4 hours ago",
+            IsNew = false,
+            LinkUrl = "#"
+        },
+        new NotificationViewModel
+        {
+            Title = "Your rights as an apprentice",
+            Description = "Find out what you're entitled to as an apprentice.",
+            TimeAgo = "4 hours ago",
+            IsNew = false,
+            LinkUrl = "#"
         }
+    };
+
+            return View(notifications);
+        }
+
+
+
+
     }
 }
---
+++ src/SFA.DAS.ApprenticeApp.Pwa/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SFA.DAS.ApprenticeApp.Domain.Models;
+
+namespace SFA.DAS.ApprenticeApp.Pwa.ViewModels
+{
+    public class NotificationViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string TimeAgo { get; set; }
+        public bool IsNew { get; set; }
+        public string LinkUrl { get; set; }
+    }
+
+}
