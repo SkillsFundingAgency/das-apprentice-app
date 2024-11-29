6b107875a8f7e96bd860653f1563bb0d05599345
tdavande <113100339+tdavande@users.noreply.github.com>
2024-11-29 13:22:28 +0000

    whitespace removed

--- src/SFA.DAS.ApprenticeApp.Pwa/Helpers/SecurityHeadersMiddleware.cs
+++ src/SFA.DAS.ApprenticeApp.Pwa/Helpers/SecurityHeadersMiddleware.cs
@@ -13,15 +13,14 @@ namespace SFA.DAS.ApprenticeApp.Pwa.Helpers
         public async Task InvokeAsync(HttpContext context)
         {
             const string dasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net";
-                context.Response.Headers.Add("Content-Security-Policy",
-                    new StringValues(
-                        $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com https://ssl.google-analytics.com ; " +
-                        $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com ; " +
-                        $"img-src {dasCdn} www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com ; " +
-                        $"font-src {dasCdn} https://fonts.gstatic.com ;" +
-                        "connect-src https://www.google-analytics.com ;" +
-                        "frame-src https://www.googletagmanager.com"));
-            
+            context.Response.Headers.Add("Content-Security-Policy",
+                new StringValues(
+                    $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com https://ssl.google-analytics.com ; " +
+                    $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com ; " +
+                    $"img-src {dasCdn} www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com ; " +
+                    $"font-src {dasCdn} https://fonts.gstatic.com ;" +
+                    "connect-src https://www.google-analytics.com ;" +
+                    "frame-src https://www.googletagmanager.com"));
             await _next(context);
         }
     }
