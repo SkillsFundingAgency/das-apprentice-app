f9c9ac542eeab9ea5e565e8bb1c89b410b68df45
tdavande <113100339+tdavande@users.noreply.github.com>
2024-12-13 12:56:57 +0000

    missing image fix for local development environment

--- src/SFA.DAS.ApprenticeApp.Pwa/Helpers/SecurityHeadersMiddleware.cs
+++ src/SFA.DAS.ApprenticeApp.Pwa/Helpers/SecurityHeadersMiddleware.cs
@@ -13,14 +13,19 @@ namespace SFA.DAS.ApprenticeApp.Pwa.Helpers
         public async Task InvokeAsync(HttpContext context)
         {
             const string dasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net";
-            context.Response.Headers.Add("Content-Security-Policy",
-                new StringValues(
-                    $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com https://ssl.google-analytics.com ; " +
-                    $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com ; " +
-                    $"img-src {dasCdn} www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com ; " +
-                    $"font-src {dasCdn} https://fonts.gstatic.com ;" +
-                    "connect-src https://www.google-analytics.com ;" +
-                    "frame-src https://www.googletagmanager.com"));
+
+            if (!context.Response.Headers.ContainsKey("Content-Security-Policy"))
+            {
+                context.Response.Headers.Add("Content-Security-Policy",
+                    new StringValues(
+                        $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com https://ssl.google-analytics.com ; " +
+                        $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com ; " +
+                        $"img-src 'self' {dasCdn} www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com ; " +
+                        $"font-src 'self' {dasCdn} https://fonts.gstatic.com https://localhost:* ; " +
+                        $"connect-src 'self' ws://localhost:* wss://localhost:* http://localhost:* https://localhost:* {dasCdn} https://www.google-analytics.com ; " +
+                        $"frame-src https://www.googletagmanager.com"));
+            }
+
             await _next(context);
         }
     }
