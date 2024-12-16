1139ebcd001ef315839b98f31c8fa5c088ba5d64
tdavande <113100339+tdavande@users.noreply.github.com>
2024-12-16 16:41:33 +0000

    updated csp for ms clarity directory

--- src/SFA.DAS.ApprenticeApp.Pwa/Helpers/SecurityHeadersMiddleware.cs
+++ src/SFA.DAS.ApprenticeApp.Pwa/Helpers/SecurityHeadersMiddleware.cs
@@ -19,12 +19,15 @@ namespace SFA.DAS.ApprenticeApp.Pwa.Helpers
         public async Task InvokeAsync(HttpContext context)
         {
             const string dasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net";
+            const string googleAnalytics = "https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com https://ssl.google-analytics.com";
+            const string clarityMs = "https://www.clarity.ms https://clarity.microsoft.com";
+            const string pstatic = "https://ssl.pstatic.com https://www.pstatic.com";
 
-            string csp = $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com https://ssl.google-analytics.com https://www.clarity.ms https://code.jquery.com ; " +
+            string csp = $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} {googleAnalytics} {clarityMs} https://code.jquery.com ; " +
                          $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com ; " +
-                         $"img-src 'self' {dasCdn} https://www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com ; " +
+                         $"img-src 'self' {dasCdn} {googleAnalytics} {pstatic} ; " +
                          $"font-src 'self' {dasCdn} https://fonts.gstatic.com ; " +
-                         $"connect-src 'self' ws://localhost:* wss://localhost:* http://localhost:* https://localhost:* {dasCdn} https://www.google-analytics.com https://www.clarity.ms ; " +
+                         $"connect-src 'self' ws://localhost:* wss://localhost:* http://localhost:* https://localhost:* {dasCdn} {googleAnalytics} {clarityMs} ; " +
                          $"frame-src https://www.googletagmanager.com";
 
             if (_env.IsDevelopment())
