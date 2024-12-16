c59f06ea9fd45f21fff4aada1459e2fed65a5575
tdavande <113100339+tdavande@users.noreply.github.com>
2024-12-16 14:29:36 +0000

    updated csp

--- src/SFA.DAS.ApprenticeApp.Pwa/Helpers/SecurityHeadersMiddleware.cs
+++ src/SFA.DAS.ApprenticeApp.Pwa/Helpers/SecurityHeadersMiddleware.cs
@@ -20,25 +20,16 @@ namespace SFA.DAS.ApprenticeApp.Pwa.Helpers
         {
             const string dasCdn = "das-at-frnt-end.azureedge.net das-pp-frnt-end.azureedge.net das-mo-frnt-end.azureedge.net das-test-frnt-end.azureedge.net das-test2-frnt-end.azureedge.net das-prd-frnt-end.azureedge.net";
 
-            string csp;
+            string csp = $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com https://ssl.google-analytics.com https://www.clarity.ms https://code.jquery.com ; " +
+                         $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com ; " +
+                         $"img-src 'self' {dasCdn} https://www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com ; " +
+                         $"font-src 'self' {dasCdn} https://fonts.gstatic.com ; " +
+                         $"connect-src 'self' ws://localhost:* wss://localhost:* http://localhost:* https://localhost:* {dasCdn} https://www.google-analytics.com https://www.clarity.ms ; " +
+                         $"frame-src https://www.googletagmanager.com";
 
             if (_env.IsDevelopment())
             {
-                csp = $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com https://ssl.google-analytics.com ; " +
-                      $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com ; " +
-                      $"img-src 'self' {dasCdn} www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com ; " +
-                      $"font-src 'self' {dasCdn} https://fonts.gstatic.com https://localhost:* ; " +
-                      $"connect-src 'self' ws://localhost:* wss://localhost:* http://localhost:* https://localhost:* {dasCdn} https://www.google-analytics.com ; " +
-                      $"frame-src https://www.googletagmanager.com";
-            }
-            else
-            {
-                csp = $"script-src 'self' 'unsafe-inline' 'unsafe-eval' {dasCdn} https://www.googletagmanager.com https://tagmanager.google.com https://www.google-analytics.com https://ssl.google-analytics.com ; " +
-                      $"style-src 'self' 'unsafe-inline' {dasCdn} https://tagmanager.google.com https://fonts.googleapis.com ; " +
-                      $"img-src 'self' {dasCdn} www.googletagmanager.com https://ssl.gstatic.com https://www.gstatic.com https://www.google-analytics.com ; " +
-                      $"font-src 'self' {dasCdn} https://fonts.gstatic.com ; " +
-                      $"connect-src 'self' {dasCdn} https://www.google-analytics.com ; " +
-                      $"frame-src https://www.googletagmanager.com";
+                csp += " http://localhost:* https://localhost:*";
             }
 
             if (!context.Response.Headers.ContainsKey("Content-Security-Policy"))
@@ -50,4 +41,3 @@ namespace SFA.DAS.ApprenticeApp.Pwa.Helpers
         }
     }
 }
-    
