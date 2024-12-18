6063a91c867fab17c60cfd54397870092b728a85
CathyGroom <147840579+CathyGroom@users.noreply.github.com>
2024-12-18 17:41:42 +0000

    add viewcomponent for counter

---
+++ src/SFA.DAS.ApprenticeApp.Pwa/ViewComponents/NotificationCounter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeApp.Application;
+using SFA.DAS.ApprenticeApp.Domain.Interfaces;
+using SFA.DAS.ApprenticeApp.Pwa.Helpers;
+
+namespace SFA.DAS.ApprenticeApp.Pwa.ViewComponents
+{
+    public class NotificationCounter : ViewComponent
+    {
+        private readonly IOuterApiClient _client;
+
+        public NotificationCounter(IOuterApiClient client)
+        {
+            _client = client;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var apprenticeId = Claims.GetClaim(HttpContext, Constants.ApprenticeIdClaimKey);
+            var notificationsResult =  await _client.GetTaskReminderNotifications(new Guid(apprenticeId));
+            if (notificationsResult != null && notificationsResult.TaskReminders.Count > 0)
+            {
+                int notificationValue = notificationsResult.TaskReminders.Count;
+                return View("../_NotificationCount", notificationValue);
+            }
+            return null;
+        }
+    }
+}
