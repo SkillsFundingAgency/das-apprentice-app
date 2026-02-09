66f6f0101f290fe7a4592ad04530228fecb9b4c5
NWake-CGI <nick.wake@cgi.com>
2026-02-09 10:44:06 +0000

    fix unit tests

--- src/SFA.DAS.ApprenticeApp.Pwa.UnitTests/Controllers/Account/AccountControllerTests.cs
+++ src/SFA.DAS.ApprenticeApp.Pwa.UnitTests/Controllers/Account/AccountControllerTests.cs
@@ -52,13 +52,7 @@ namespace SFA.DAS.ApprenticeApp.Pwa.UnitTests.Controllers.Account
             var result = controller.Authenticated();
 
             using (new AssertionScope())
-            {
-                logger.Verify(x => x.Log(LogLevel.Information,
-                   It.IsAny<EventId>(),
-                   It.Is<It.IsAnyType>((object v, Type _) =>
-                           v.ToString().Contains($"Apprentice authenticated and cookies added for")),
-                   It.IsAny<Exception>(),
-                   (Func<It.IsAnyType, Exception, string>)It.IsAny<object>()));
+            {                
                 result.Should().NotBeNull();
             }
         }
@@ -85,7 +79,7 @@ namespace SFA.DAS.ApprenticeApp.Pwa.UnitTests.Controllers.Account
             var result = await controller.Authenticated() as RedirectToActionResult;
 
             // Assert
-            result!.ActionName.Should().Be("EmailMismatchError");
+            result!.ActionName.Should().Be("AccountNotFound");
             result.ControllerName.Should().Be("Account");
         }
 
@@ -112,8 +106,8 @@ namespace SFA.DAS.ApprenticeApp.Pwa.UnitTests.Controllers.Account
 
             client.Setup(client => client.GetApprenticeDetails(It.IsAny<Guid>())).ReturnsAsync(new ApprenticeDetails() { MyApprenticeship = null });
             var result = await controller.Authenticated() as RedirectToActionResult;
-            result.ActionName.Should().Be("CmadError");
-            result.ControllerName.Should().Be("Cmad");
+            result.ActionName.Should().Be("AccountNotFound");
+            result.ControllerName.Should().Be("Account");
         }
         //  
 
