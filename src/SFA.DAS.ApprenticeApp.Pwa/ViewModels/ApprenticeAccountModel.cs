76027d73a67eac22716cf9c5be2ab9a2052641d0
nnagepat <nilesh_nagepatil@yahoo.co.in>
2026-02-11 15:03:45 +0000

    Apprentice Account page – added support for performance indicators

---
+++ src/SFA.DAS.ApprenticeApp.Pwa/ViewModels/ApprenticeAccountModel.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.ApprenticeApp.Domain.Models;
+
+namespace SFA.DAS.ApprenticeApp.Pwa.ViewModels
+{
+    public class  ApprenticeAccountModel
+    {
+       public ApprenticeKsbsPageModel? apprenticeKsbsPageModel { get; set; }
+    }
+}
